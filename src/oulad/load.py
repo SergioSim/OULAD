e8ff3551bce68048249de8eebf67cf7f3ff512d1
SergioSim <Sergio.Simonian@etu.unice.fr>
2023-05-05 15:05:16 +0200

    ✨(oulad) make the get_oulad function remove unlinked records
    Some vle resources and assessments have no related links in the
    OULAD dataset. We want to remove them.

--- src/oulad/load.py
+++ src/oulad/load.py
@@ -24,8 +24,24 @@ class OULAD:
 def get_oulad(path: str = "/app/OULAD") -> OULAD:
     """Returns the OULAD dataset tables in a dataclass."""
 
+    # Remove unlinked assessments (they don't have any corresponding student_assessment)
+    assessments = pd.read_csv(f"{path}/assessments.csv")
+    assessments.drop(
+        assessments[assessments.id_assessment.isin([40088, 40087])].index, inplace=True
+    )
+
+    # Remove unlinked vle resources (they don't have any corresponding student_vle)
+    # Also dropping `week_from` and `week_to` columns as most of the values are missing.
+    vle = pd.read_csv(f"{path}/vle.csv")
+    student_vle = pd.read_csv(f"{path}/studentVle.csv")
+    resources = pd.Index(student_vle.id_site.unique())
+    vle = vle.loc[
+        ~vle.id_site.isin(resources.symmetric_difference(vle.id_site.values)),
+        ["id_site", "code_module", "code_presentation", "activity_type"],
+    ]
+
     return OULAD(
-        assessments=pd.read_csv(f"{path}/assessments.csv"),
+        assessments=assessments,
         courses=pd.read_csv(f"{path}/courses.csv"),
         domains=pd.DataFrame(
             {
@@ -44,6 +60,6 @@ def get_oulad(path: str = "/app/OULAD") -> OULAD:
         student_assessment=pd.read_csv(f"{path}/studentAssessment.csv"),
         student_info=pd.read_csv(f"{path}/studentInfo.csv"),
         student_registration=pd.read_csv(f"{path}/studentRegistration.csv"),
-        student_vle=pd.read_csv(f"{path}/studentVle.csv"),
-        vle=pd.read_csv(f"{path}/vle.csv"),
+        student_vle=student_vle,
+        vle=vle,
     )
